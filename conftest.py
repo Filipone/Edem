4f081e161e95f9aab7b06e0645cfb7bd197c411d
Filipp <bobrovich.fil@mail.ru>
2020-03-23 12:54:42 +0300

    Add Edem project

---
+++ conftest.py
@@ -0,0 +1,56 @@
+import time
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    """Открытие определенного браузера по умолчанию"""
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+
+LINK_MAIN = "https://edem-shop.by/catalog/bukety/"
+
+
+@pytest.fixture(scope="session")
+def browser(request):
+    """Запускаем определенный браузер, добавляем товар в корзину, выбираем дату
+        request нужен для выбора браузера"""
+    print("\nStart")
+    br_name = request.config.getoption("--browser_name")
+    if br_name == "chrome":
+        print("\nstart chrome browser for test..")
+        br = webdriver.Chrome()
+    elif br_name == "firefox":
+        print("\nstart firefox browser for test..")
+        br = webdriver.Firefox()
+    else:
+        raise pytest.UsageError(f"--browser_name {br_name}, should be chrome or firefox")
+    br.implicitly_wait(5)
+
+
+    # Переход на сайт
+    br.get(LINK_MAIN)
+    time.sleep(2)
+
+    br.find_element_by_css_selector("div.buts.clearfix>a.add_to").click()
+    br.find_element_by_css_selector("a.zakaz").click()
+    time.sleep(2)
+    br.execute_script("window.scrollBy(0, 400);")
+
+    # Работа в корзине
+    time.sleep(1)
+    br.find_element_by_css_selector("p.delivery-form__text").click()  # Выбор доставки
+    br.find_element_by_css_selector("input#new_order_date").click()  # Выбор дня
+    br.execute_script("window.scrollBy(0, 500);")
+    time.sleep(1)
+    # br.find_element_by_css_selector("a.ui-state-default.ui-state-highlight.ui-state-active").click()
+
+    # Выбор 8 марта
+    br.find_element_by_css_selector("span.ui-icon.ui-icon-circle-triangle-e").click()
+    time.sleep(1)
+    br.find_element_by_link_text('8').click()
+
+    yield br
+    print("\nfinish")
+    br.quit()
---
+++ test_edem2_0.py
@@ -0,0 +1,100 @@
+import time
+import pytest
+
+CITIES_20_BYN = [
+    "Белая Лужа", "Глинище", pytest.param("Хрень", marks=pytest.mark.xfail),
+    "Прудок", "Красный Октябрь", "Новосёлки", "Новосёлки", "Старая Мётча",
+    "Белое Болото", "Старина", "Брили", "Костюки", "Жерствянка",
+    "Лавники", "Каменка", "Большая Тростяница", "Малая Тростяница",
+    "Заболотье", "Стрелковцы", "Тимки", "Садовщина", "Пасека",
+    "Блонь", "Юзефово", "Лесная поляна", "Раковцы", "Судоль", "Будагово",
+    "Михайлово", "Михайлово, неманицкий сельсовет", "Лобачиха",
+    "Домостроитель", "Ново-лесуны", "Швейник-2011", "Фруктовый",
+    "Кургановка", "Ланковщина", "Зорька", "Ветеран-Шилино", "Шилино",
+    "Гребло", "Ярцевка", "Зелёная Дубрава", "Аскерки", "Добрицкое",
+    "Завалы", "Сивица", "Рубленики", "Новый Посёлок", "Барановка",
+    "Перстень", "Козловка", "Красное", "Забашевка", "Застенок",
+    "Слободка", "Смолье", "Заручье", "Дружный Ландыш", "Криница-88",
+    "Строитель-Тарасики", "Дзержинец", "Лесная Поляна-88", "Аккорд-88",
+    "Восход", "Экран-Тарасики", "Тарасики", "Солнечный-2", "Меркурий-2002",
+    "Патриот-Берёзка", "Тарасик", "Прогресс-2001", "Бродовка", "Веселово",
+    "Забашевичи", "Зембин", "Кищина Слобода", "Кострица", "Кричино", "Лошница",
+    "Ляховка", "Новая Мётча", "Новосады"
+]
+
+@pytest.mark.parametrize("city", CITIES_20_BYN)
+@pytest.mark.smoke
+def test_cities20byn_validation(browser, city):
+    """Проверка валидации и стоимости городов из списка за 20руб"""
+    input_city = browser.find_element_by_css_selector("input#locality")
+    input_city.clear()
+    input_city.send_keys(city)
+
+    otvet = browser.find_element_by_css_selector("input#locality + span.fields__text")
+    otvet.click()
+    time.sleep(1)
+    delivary_prise = browser.find_element_by_css_selector("span.js_delivery").text
+
+    print(city)
+    assert "Город, посёлок" in browser.find_element_by_css_selector(
+        "input#locality + span.fields__text").text, f"Валидацию не прошел город: {city}"
+
+    print(delivary_prise)
+    assert "20,00" in delivary_prise, "Стоимсоть доставки не верна"
+
+
+CITIES_10_BYN = [
+    "Большое Стахово", "Большая Ухолода", "Бытча", "Гливин", "Дубовый лог",
+    "Житьково", "Жодино", "Изобка", "Малое Стахово", "Малая Ухолода", "Неманица",
+    "Пересады", "Погодица", "Подберезье", "Светлая Роща", "Стайки", "Студёнка",
+    "Упиревичи", "Лещины", "Дубени", "Селище", "Любатовщина", "Прудище", "Брусы",
+    "Горелица", "Залесье", "Струпень", "Ельница", "Верески", "Круглое", "Селитренка",
+    "Липки", "Стаи", "Проходы", "Проходы"
+]
+
+
+@pytest.mark.parametrize("city", CITIES_10_BYN)
+def test_cities10byn_validation(browser, city):
+    """Проверка валидации и стоимости городов из списка за 10руб"""
+    input_city = browser.find_element_by_css_selector("input#locality")
+    input_city.clear()
+    input_city.send_keys(city)
+
+    otvet = browser.find_element_by_css_selector("input#locality + span.fields__text")
+    otvet.click()
+    time.sleep(1)
+    delivary_prise = browser.find_element_by_css_selector("span.js_delivery").text
+
+    print(city)
+    assert "Город, посёлок" in browser.find_element_by_css_selector(
+        "input#locality + span.fields__text").text, f"Валидацию не прошел город: {city}"
+
+    print(delivary_prise)
+    assert "10,00" in delivary_prise, "Стоимсоть доставки не верна"
+
+
+CITIES_5_BYN = [
+    'Борисов', 'Пчельник', 'Остров', 'Дудинка', 'Печи', 'Тарасовка',
+    'Углы', 'Гора', 'Старо Борисов', 'Демидовка'
+]
+
+
+@pytest.mark.parametrize("city", CITIES_5_BYN)
+def test_cities5byn_validation(browser, city):
+    """Проверка валидации и стоимости городов из списка за 5руб"""
+    input_city = browser.find_element_by_css_selector("input#locality")
+    input_city.clear()
+    input_city.send_keys(city)
+
+    otvet = browser.find_element_by_css_selector("input#locality + span.fields__text")
+    otvet.click()
+    time.sleep(1)
+    delivary_prise = browser.find_element_by_css_selector("span.js_delivery").text
+
+    try:
+        print(city)
+        print(delivary_prise)
+        assert "Город, посёлок" in browser.find_element_by_css_selector(
+            "input#locality + span.fields__text").text, f"Валидацию не прошел город: {city}"
+    finally:
+        assert "5,00" in delivary_prise, f"Стоимость доставки у города {city} не верна"
